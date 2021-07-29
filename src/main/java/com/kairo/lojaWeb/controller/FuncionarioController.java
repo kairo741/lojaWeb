5c0e5b61c1804986666bb64e68cb8d56ac00a571
Kairo <kairo.controlenamao@gmail.com>
2021-07-28 23:31:51 -0300

    Tudo até a aula 11 feito, mais algumas coisas extras

--- src/main/java/com/kairo/lojaWeb/controller/FuncionarioController.java
+++ src/main/java/com/kairo/lojaWeb/controller/FuncionarioController.java
@@ -4,11 +4,15 @@ import com.kairo.lojaWeb.models.Funcionario;
 import com.kairo.lojaWeb.repositories.FuncionarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+
 import javax.validation.Valid;
+import java.util.Optional;
 
 @Controller
 public class FuncionarioController {
@@ -17,25 +21,46 @@ public class FuncionarioController {
     private FuncionarioRepository funcionarioRepository;
 
     @GetMapping("/administrativo/funcionarios/cadastrar")
-    public ModelAndView register (Funcionario funcionario) {
+    public ModelAndView register(Funcionario funcionario) {
         ModelAndView mv = new ModelAndView("administrativo/funcionarios/cadastro");
         mv.addObject("funcionario", funcionario);
         return mv;
     }
 
     @GetMapping("/administrativo/funcionarios/listar")
-    public String list() {
-        return "administrativo/funcionarios/lista";
+    public ModelAndView list() {
+        ModelAndView mv = new ModelAndView("administrativo/funcionarios/lista");
+        mv.addObject("funcionariosList", funcionarioRepository.findAll());
+        return mv;
+    }
+
+    @GetMapping("/administrativo/funcionarios/editar/{id}")
+    public ModelAndView edit(@PathVariable("id") Long id) {
+        Optional<Funcionario> funcionarioOptional = funcionarioRepository.findById(id);
+        return register(funcionarioOptional.get());
+    }
+
+    @GetMapping("/administrativo/funcionarios/remover/{id}")
+    public ModelAndView remove(@PathVariable("id") Long id) {
+        Optional<Funcionario> funcionarioOptional = funcionarioRepository.findById(id);
+        funcionarioRepository.delete(funcionarioOptional.get());
+        return list();
+    }
+
+    @GetMapping("/administrativo/funcionarios/remover")
+    public ModelAndView removeAll() {
+        funcionarioRepository.deleteAll();
+        return list();
     }
 
     @PostMapping("/administrativo/funcionarios/salvar")
-    public ModelAndView save (@Valid Funcionario funcionario, BindingResult result) {
-
-        if(result.hasErrors()){
+    public ModelAndView save(@Valid Funcionario funcionario, BindingResult result) {
+        if (result.hasErrors()) {
             return register(funcionario);
         }
+//        funcionario.setSenha(new BCryptPasswordEncoder().encode(funcionario.getSenha())); todo - enriptar senha con o Secutiry
         funcionarioRepository.save(funcionario);
-        return register(new Funcionario());
+        return list();
     }
 
 }
--- src/main/java/com/kairo/lojaWeb/repositories/FuncionarioRepository.java
+++ src/main/java/com/kairo/lojaWeb/repositories/FuncionarioRepository.java
@@ -3,5 +3,11 @@ package com.kairo.lojaWeb.repositories;
 import com.kairo.lojaWeb.models.Funcionario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
+
+    List<Funcionario> findAll();
+
+
 }
