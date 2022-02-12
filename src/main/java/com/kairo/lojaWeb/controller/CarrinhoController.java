9134683aa524b8442481f8c50c6613e5567815ce
Kairo <kairo.controlenamao@gmail.com>
2022-02-12 12:08:38 -0300

    Aula 37

--- src/main/java/com/kairo/lojaWeb/controller/CarrinhoController.java
+++ src/main/java/com/kairo/lojaWeb/controller/CarrinhoController.java
@@ -1,14 +1,23 @@
 package com.kairo.lojaWeb.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 
+@Slf4j
 @Controller
 public class CarrinhoController {
 
     @GetMapping("/carrinho")
     public ModelAndView carrinho() {
+        return new ModelAndView("cliente/carrinho");
+    }
+
+    @GetMapping("/addCarrinho/{id}")
+    public ModelAndView carrinho(@PathVariable Long id) {
+        log.debug("Id do produto: {}", id);
         return new ModelAndView("cliente/carrinho");
     }
 
